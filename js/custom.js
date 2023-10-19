6d4d37a3840b83e910cec98c704f3abb752d70b9
abdallahnegm <abdallahnegm5@gmail.com>
2023-10-19 04:19:18 +0300

    start0
    start

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+let btn= document.getElementById("check");
+let menu = document.getElementById("menu1");
+btn.onclick=function (){
+    if(menu.style.display == "none"){
+        menu.style.display="flex";
+    }
+    else{
+        menu.style.display ="none";
+            }}
+
+
+
